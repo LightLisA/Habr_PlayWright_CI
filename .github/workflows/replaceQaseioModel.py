02b0014bc918248f45105f2227bd3fdeb336cc4f
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2024-09-30 22:03:12 +0300

    Update replaceQaseioModel.py

--- .github/workflows/replaceQaseioModel.py
+++ .github/workflows/replaceQaseioModel.py
@@ -1,4 +1,4 @@
-import os
+import sys
 
 def replace_in_file(file_path, search_text, replace_text):
     with open(file_path, 'r') as file:
@@ -9,7 +9,7 @@ def replace_in_file(file_path, search_text, replace_text):
     with open(file_path, 'w') as file:
         file.write(new_content)
 
-# Знаходимо файл у вашій віртуальній машині
-path_to_file = 'path_to_your_virtualenv/site-packages/qaseio/commons/testops.py'
+# Отримуємо шлях до файлу як аргумент
+path_to_file = sys.argv[1]
 
 replace_in_file(path_to_file, 'from qaseio.model', 'from qaseio.models')

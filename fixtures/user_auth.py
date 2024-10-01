7e17fdb6c3a03a5c2ca9a937b2031c0f59faca1d
Oleksii Lysytskyi <o.lysytskyi@aibuy.io>
2024-10-01 11:14:51 +0300

    Add print for debugging

--- fixtures/user_auth.py
+++ fixtures/user_auth.py
@@ -5,4 +5,5 @@ from pages.main_page import Main
 @pytest.fixture(scope='class')
 def user_login(browser):
     m = Main(browser)
+    print(f"\n=== User Login from user_login.py ===")
     m.user_login()
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -5,6 +5,8 @@ from pages.market_main_page import MarketPage
 @pytest.mark.regression
 @pytest.mark.usefixtures('user_login')
 class TestBuyProduct:
+    print(f"\n=== TEST BUY PRODUCT from test_buy_product.py ===")
+
     def test_buy_product(self, browser):
         p = MarketPage(browser)
         p.add_to_cart()
